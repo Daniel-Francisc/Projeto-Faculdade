bd27798be2619d6d598b78ee991dbd994f26ef86
Francisco Daniel <francisco.santos@undf.edu.br>
2025-03-28 18:07:00 -0300

    Objetos

---
+++ model/ConexaoUsuario.class.php
@@ -0,0 +1,23 @@
+<?php
+class ConexaoUsuario{
+    #region Atributos
+        private $host = 'localhost';
+        private $db = 'db_usuarioLogin';
+        private $user = 'root';
+        private $psw = '';
+        private $link = null;
+    #endregion
+
+    #region Métodos
+    public function conectarUsuario(){
+        try{
+            $pdo = new PDO("mysql:host={$this->host};dbname={$this->db}", "{$this->user}", "{$this->psw}");
+            $this->link=$pdo;
+            print 'Conectado!';
+            return $this->link;
+        }
+        catch(PDOException $e){
+            print $e;
+        }
+    }
+}
