1aa41ca07ab72ea140ffb3f2c0f7ae6a0da63cd0
Daniel-Francisc <francisco.santos@undf.edu.br>
2025-04-23 21:20:47 -0300

    Api

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-//error_reporting(~E_ALL & ~E_NOTICE & ~E_WARNING);
+error_reporting(~E_ALL & ~E_NOTICE & ~E_WARNING);
 
 include_once 'autoload.php';
 include_once 'router.php';
